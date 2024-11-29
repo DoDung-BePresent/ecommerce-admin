019ab98bbd06c3387b69362c4fa22146cb68cc21
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2024-11-29 23:10:23 +0700

    dashboard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
 
 import "./globals.css";
-
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,9 +36,11 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ToasterProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
